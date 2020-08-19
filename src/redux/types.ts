4ac33eca7d9de491a89635fc71a7dc15a1a0d4b1
Michel <michelpm1@gmail.com>
2020-08-19 20:24:41 -0300

    Add router, Add Progress and Question redux Types

---
+++ src/redux/types.ts
@@ -0,0 +1,19 @@
+export interface ProgressState {
+    status: QuestionsState,
+}
+
+export interface Question {
+    category: string,
+    type: string,
+    difficulty: string,
+    question: string,
+    rightAnswer: boolean,
+    answer?: boolean
+}
+
+export interface QuestionsState {
+    questions: Question[],
+    correctQuestions: number,
+    incorrectQuestions: number,
+    progress: string,
+}
---
+++ src/router.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { useSelector } from 'react-redux'
+import { ProgressState } from './redux/types'
+
+const Router = () => {
+    const actualState = useSelector((state: ProgressState) => {
+        const { status } = state;
+        return status.progress
+    });
+
+    switch (actualState) {
+        case 'finished':
+            return <EndPage />
+
+        case 'started':
+            return <QuestionPage />
+
+        default:
+            return <IntroPage />
+    }
+}
+
+export default Router;
