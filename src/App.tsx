9cb1ab3cdee72780cade77673895adff469608eb
Michel <michelpm1@gmail.com>
2020-08-22 17:38:49 -0300

    Finish layout introPage component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import Router from './router'
+import './App.css'
 
 const App = () =>
   <Provider store={store}>
--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -8,10 +8,10 @@ const IntroPage = () => {
     return (
         <Layout>
             <h1 className={styles.title}>Welcome to the trivia challenge!</h1>
-            <p className={styles.firstText}>You will be presented with 10 True or False questions!</p>
-            <p className={styles.secondText}>Will you score 100%?</p>
+            <p className={styles.defaultText}>You will be presented with 10 True or False questions!</p>
+            <p className={styles.defaultText}>Will you score 100%?</p>
             <div className={styles.buttonBox}>
-                <button onClick={() => dispatch(startApp())}>Begin</button>
+                <button className={styles.introButton} onClick={() => dispatch(startApp())}>Begin</button>
             </div>
         </Layout>
     )
