727b45242137c4dffdf782789229fb60c09bb736
Michel <michelpm1@gmail.com>
2020-08-22 19:21:12 -0300

    Add test to render App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Should App render', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Welcome to the trivia challenge!/i);
   expect(linkElement).toBeInTheDocument();
 });
