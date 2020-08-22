6719569ae491bce88828a6f96e61ee301d3585ca
Michel <michelpm1@gmail.com>
2020-08-22 18:23:05 -0300

    Add Storybook, Add IntroPage.stories

---
+++ src/pages/IntroPage/IntroPage.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Provider } from 'react-redux'
+import store from '../../redux/store'
+import IntroPage from './IntroPage'
+
+const stories = storiesOf('IntroPage', module);
+stories.add('IntroPage', () =>
+    <Provider store={store}>
+        <IntroPage />
+    </Provider>
+)
