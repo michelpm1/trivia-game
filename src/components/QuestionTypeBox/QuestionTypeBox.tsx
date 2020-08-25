d7a33eb74e52b45d62717a3522b2559ec9d41f16
Michel <michelpm1@gmail.com>
2020-08-24 21:33:01 -0300

    Finish layout for questionBox

--- src/components/QuestionTypeBox/QuestionTypeBox.tsx
+++ src/components/QuestionTypeBox/QuestionTypeBox.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-
+import styles from './QuestionTypeBox.module.css';
 interface QuestionProps {
     children: any,
 }
 
 const QuestionTypeBox = (props: QuestionProps) => {
     return (
-        <div>
-            {props.children}
+        <div className={styles.questionBox}>
+            <span className={styles.questionBoxText}> {props.children}</span>
         </div>
     )
 }
