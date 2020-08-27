65f06e9d90b8235665c49310fb9b95e4e369bcac
Michel <michelpm1@gmail.com>
2020-08-26 23:54:59 -0300

    Rename some components to Upper_case, Add docs

---
+++ src/components/DefaultLoading/DefaultLoading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+import styles from './DefaultLoading.module.css';
+
+const DefaultLoading = () => (
+  <div className={styles.defaultLoading}>
+    <ReactLoading type='spokes' color='black' height={200} width={100} />
+  </div>
+);
+
+export default DefaultLoading;
